19937f2e306038be1a689b7a1f5c9b6606a24eaa
Kenneth Li <kennethli.3470@gmail.com>
2017-03-30 14:24:25 -0400

    Directory reshuffle

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,5 @@ var heatmap = document.getElementById('heatmap');
 var bbox = heatmap.getBBox();
 var viewBox = bbox.x + ' ' + bbox.y + ' ' + bbox.width + ' ' + bbox.height;
 heatmap.setAttribute('viewBox', viewBox);
+
+var states = heatmap.
