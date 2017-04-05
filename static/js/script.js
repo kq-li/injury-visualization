e6eb5ad0ed36d09db16f102dd7fad219b24b5a35
Kenneth Li <kennethli.3470@gmail.com>
2017-04-05 01:30:30 -0400

    tooltip done

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,13 @@
-Element.prototype.setAttributes = function (attributes) {
-    for (var attribute in attributes) {
-        this.setAttribute(attribute, attributes[attribute]);
-    }
-}
-
 var heatmap = $('#heatmap');
 var heatmapBBox = heatmap[0].getBBox();
 var viewBox = heatmapBBox.x + ' ' + heatmapBBox.y + ' ' + heatmapBBox.width + ' ' + heatmapBBox.height;
 heatmap.attr('viewBox', viewBox);
 
-var states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+var stateAbvs = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+
+var stateNames = ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kansas', 'Kentucky', 'Louisiana', 'Massachusetts', 'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 'Mississippi', 'Montana', 'North Carolina', 'North Dakota', 'Nebraska', 'New Hampshire', 'New Jersey', 'New Mexico', 'Nevada', 'New York', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Virginia', 'Vermont', 'Washington', 'Wisconsin', 'West Virginia', 'Wyoming'];
+
+var stateChart = {'AK': 'Alaska', 'AL': 'Alabama', 'AR': 'Arkansas', 'AZ': 'Arizona', 'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut', 'DC': 'District of Columbia', 'DE': 'Delaware', 'FL': 'Florida', 'GA': 'Georgia', 'HI': 'Hawaii', 'IA': 'Iowa', 'ID': 'Idaho', 'IL': 'Illinois', 'IN': 'Indiana', 'KS': 'Kansas', 'KY': 'Kentucky', 'LA': 'Louisiana', 'MA': 'Massachusetts', 'MD': 'Maryland', 'ME': 'Maine', 'MI': 'Michigan', 'MN': 'Minnesota', 'MO': 'Missouri', 'MS': 'Mississippi', 'MT': 'Montana', 'NC': 'North Carolina', 'ND': 'North Dakota', 'NE': 'Nebraska', 'NH': 'New Hampshire', 'NJ': 'New Jersey', 'NM': 'New Mexico', 'NV': 'Nevada', 'NY': 'New York', 'OH': 'Ohio', 'OK': 'Oklahoma', 'OR': 'Oregon', 'PA': 'Pennsylvania', 'RI': 'Rhode Island', 'SC': 'South Carolina', 'SD': 'South Dakota', 'TN': 'Tennessee', 'TX': 'Texas', 'UT': 'Utah', 'VA': 'Virginia', 'VT': 'Vermont', 'WA': 'Washington', 'WI': 'Wisconsin', 'WV': 'West Virginia', 'WY': 'Wyoming'};
 
 var injuryCounts = [];
 var populations = [];
@@ -27,7 +25,7 @@ function fillHeatmap(counts) {
 
     var paths = d3.selectAll('path');
     paths.data(counts).style('fill', function (d) {
-        var lightness = 95 - 50 * d[1] / maxCount;
+        var lightness = 100 - 55 * d[1] / maxCount;
         return 'hsl(0, 100%,' + lightness + '%)';
     });
 }
@@ -70,12 +68,37 @@ $('#heatmap-data').change(function () {
 });
 
 $(document).ready(function () {
+    if (injuryFractions.length == 0) {
+        $.post('/data/injury_counts', function (injuryData) {
+            injuryCounts = JSON.parse(injuryData);
+            
+            $.post('/data/populations', function (populationData) {
+                populations = JSON.parse(populationData);
+
+                for (var i = 0; i < injuryCounts.length; i++) {
+                    injuryFractions.push(
+                        [injuryCounts[i][0],
+                         injuryCounts[i][1] * 10000000.0 / populations[i][1]]);
+                }
+
+                fillHeatmap(injuryFractions);
+            });
+        });
+    }
+    
+    if (daysAway.length == 0) {
+        $.post('/data/average_days_away', function (daysAwayData) {
+            daysAway = JSON.parse(daysAwayData);
+            fillHeatmap(daysAway);
+        });
+    }
+    
     loadHeatmap($('#heatmap-data').val());
 });
 
 var tooltip = $(document.createElementNS('http://www.w3.org/2000/svg', 'g'));
 var tooltipRect = $(document.createElementNS('http://www.w3.org/2000/svg', 'rect')).attr({
-    id: 'tooltip',
+    class: 'tooltip-rect',
     x: 0,
     y: 0,
     width: 100,
@@ -84,11 +107,25 @@ var tooltipRect = $(document.createElementNS('http://www.w3.org/2000/svg', 'rect
     ry: 10
 });
 
-var tooltipText = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'));
+var tooltipState = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+    .attr('class', 'tooltip-state');
+var tooltipInjuries = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+    .attr('class', 'tooltip-injuries');
+var tooltipPopulation = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+    .attr('class', 'tooltip-population');
+var tooltipFraction = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+    .attr('class', 'tooltip-fraction');
+var tooltipDaysAway = $(document.createElementNS('http://www.w3.org/2000/svg', 'text'))
+    .attr('class', 'tooltip-days-away');
 var TOOLTIP_SPACING = 20;
 var TOOLTIP_PADDING = 10;
 
-tooltip.append(tooltipRect).append(tooltipText);
+tooltip.append(tooltipRect)
+    .append(tooltipState)
+    .append(tooltipInjuries)
+    .append(tooltipPopulation)
+    .append(tooltipFraction)
+    .append(tooltipDaysAway);
 tooltip.hide();
 heatmap.append(tooltip);
 
@@ -108,11 +145,30 @@ $('#heatmap').on('mousemove', function (e) {
         x: viewBoxCoords[0] + TOOLTIP_SPACING,
         y: viewBoxCoords[1] + TOOLTIP_SPACING
     });
+    
+    tooltipState.attr({
+        x: parseInt(tooltipRect.attr('x')) + TOOLTIP_PADDING,
+        y: parseInt(tooltipRect.attr('y')) + TOOLTIP_PADDING + tooltipState[0].getBBox().height
+    });
 
-    tooltipText.attr({
-        x: viewBoxCoords[0] + TOOLTIP_SPACING + TOOLTIP_PADDING,
-        y: viewBoxCoords[1] + tooltipText[0].getBBox().height * 0.75
-            + TOOLTIP_SPACING + TOOLTIP_PADDING
+    tooltipInjuries.attr({
+        x: parseInt(tooltipState.attr('x')),
+        y: parseInt(tooltipState.attr('y')) + TOOLTIP_PADDING + tooltipInjuries[0].getBBox().height
+    });
+
+    tooltipPopulation.attr({
+        x: parseInt(tooltipInjuries.attr('x')),
+        y: parseInt(tooltipInjuries.attr('y')) + TOOLTIP_PADDING + tooltipPopulation[0].getBBox().height
+    });
+
+    tooltipFraction.attr({
+        x: parseInt(tooltipPopulation.attr('x')),
+        y: parseInt(tooltipPopulation.attr('y')) + TOOLTIP_PADDING + tooltipFraction[0].getBBox().height
+    });
+
+    tooltipDaysAway.attr({
+        x: parseInt(tooltipFraction.attr('x')),
+        y: parseInt(tooltipFraction.attr('y')) + TOOLTIP_PADDING + tooltipDaysAway[0].getBBox().height
     });
 });
 
@@ -228,16 +284,37 @@ function makeDonutChart(data, width, height, radius, inner) {
     return chart;
 }
 
-function hoverState(state) {
-    console.log('hover' + state.getAttribute('id'));
-    state.style['z-index'] = 1;
-    state.style['stroke-width'] = 3;
+function hoverState(statePath) {
+    console.log('hover' + statePath.getAttribute('id'));
+    statePath.style['z-index'] = 1;
+    statePath.style['stroke-width'] = 3;
 
-    tooltipText.text('test');
+    var stateIndex = stateAbvs.indexOf(statePath.getAttribute('id').replace('US-', ''));
+    var stateName = stateNames[stateIndex];
+    var stateInjuries = injuryCounts[stateIndex][1];
+    var statePopulation = populations[stateIndex][1];
+    var stateFraction = parseInt(stateInjuries * 10000000 / statePopulation)
+    var stateDaysAway = daysAway[stateIndex][1];
 
+    tooltipState.text(stateName);
+    tooltipInjuries.text('Injuries: ' + stateInjuries);
+    tooltipPopulation.text('Population: ' + statePopulation.toLocaleString('en-US'));
+    tooltipFraction.text('Per 10M: ' + stateFraction);
+    tooltipDaysAway.text('Avg days away: ' + stateDaysAway.toLocaleString('en-US', {
+        maximumFractionDigits: 2
+    }));
+
+    var width = Math.max(tooltipState[0].getBBox().width,
+                         tooltipInjuries[0].getBBox().width,
+                         tooltipPopulation[0].getBBox().width,
+                         tooltipFraction[0].getBBox().width,
+                         tooltipDaysAway[0].getBBox().width);
+    var height = tooltipState[0].getBBox().height + tooltipInjuries[0].getBBox().height
+        + tooltipPopulation[0].getBBox().height + tooltipFraction[0].getBBox().height
+        + tooltipDaysAway[0].getBBox().height;
     tooltipRect.attr({
-        width: tooltipText[0].getBBox().width + 2 * TOOLTIP_PADDING,
-        height: tooltipText[0].getBBox().height + 2 * TOOLTIP_PADDING
+        width: width + 2 * TOOLTIP_PADDING,
+        height: height + 6 * TOOLTIP_PADDING
     });
 
     tooltip.show();
@@ -261,7 +338,7 @@ function deselectState(state) {
     state.style['stroke-width'] = 1;
 }
 
-$('path').each(function (i, state) {
+$('#heatmap path').each(function (i, state) {
     state.addEventListener('mouseover', function (e) {
         hoverState(this);
     });
@@ -307,6 +384,6 @@ function post(element, state) {
 };
 
 var i;
-for(i = 0 ; i < states.length ; i++){
-    document.getElementById('US-' + states[i]).addEventListener('click', post(document.getElementById('US-' + states[i]), states[i]));
+for(i = 0 ; i < stateAbvs.length ; i++){
+    document.getElementById('US-' + stateAbvs[i]).addEventListener('click', post(document.getElementById('US-' + stateAbvs[i]), stateAbvs[i]));
 }
