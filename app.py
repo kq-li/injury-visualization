0c63a7b12cfbb7a52be28881fd93dce0577828f4
wostlund <wostlund@stuy.edu>
2017-04-05 00:44:05 -0400

    Fixed app again

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from data import process, group
+from data import process
 from flask import Flask, render_template, request, session, url_for, redirect
 import json
 
@@ -31,7 +31,7 @@ def state_industries(state):
 @app.route("/state", methods = ["GET"])
 def post():
     data = request.args.get("text")
-    result = group.getStateData(data);   
+    result = process.get_state_industries(data);   
     return json.dumps(result)
     
 if __name__ == '__main__':
